3ee92c60347fce2e8e07be5dd693db46a38770e6
shabihhaider <2022cs723@student.uet.edu.pk>
2025-02-08 17:54:27 -0800

    Final Touch

--- src/components/MoiveCard.jsx
+++ src/components/MoiveCard.jsx
@@ -5,7 +5,7 @@ const MoiveCard = ({movie:
 }) => {
   return (
     <div className='movie-card'>
-        <img src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : '/no-movie.png'} alt='title' />
+        <img src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : '/no-movie.png'} alt={title} />
 
         <div className='mt-4'>
             <h3>{title}</h3>
